3c5f1254b9d07e717e253a0c23f568c368845316
Andrew Eggleston <egglestonandrew927@gmail.com>
2025-09-06 00:07:23 -0400

    add plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+
+_G.theme = "paradise"
+
+require("config.options")
+require("config.keybinds")
+require("config.lazy")
+require("config.ui")
+
+-- Prefer LSP folding if client supports it. Allows to fold comments and more
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    if client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,81 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local o = vim.opt
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Better UX
+o.autowriteall = true
+o.splitright = true
+o.splitbelow = true
+o.swapfile = false
+o.undofile = true
+o.backup = false
+o.writebackup = false
+o.clipboard = "unnamedplus"
+o.scrolloff = 10
+
+o.cursorline = true
+o.cursorlineopt = "screenline"
+o.cursorcolumn = true
+
+-- Indents settings
+o.tabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+o.smartindent = true
+o.autoindent = true
+o.copyindent = true
+o.breakindent = true
+
+-- UI
+o.showcmd = false
+-- Relative line number insert mode
+o.number = true
+vim.api.nvim_create_autocmd(
+	{ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
+	{ pattern = "*", command = "if &nu && mode() != 'i' | set rnu | endif" }
+)
+vim.api.nvim_create_autocmd(
+	{ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
+	{ pattern = "*", command = "if &nu | set nornu | endif" }
+)
+-- o.relativenumber = true
+o.showtabline = 1
+o.signcolumn = "yes"
+o.breakindent = true
+vim.g.termguicolors = true
+o.updatetime = 500
+o.showmode = false
+o.laststatus = 3
+o.fillchars = { eob = " " }
+vim.go.guicursor = "n-v-sm:block,i-t-ci-ve-c:ver25,r-cr-o:hor20"
+-- Search
+o.ignorecase = true
+o.smartcase = true
+o.inccommand = "split"
+
+vim.diagnostic.config({
+	virtual_text = true,
+	float = { border = "rounded" },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+	},
+})
+
+-- https://www.reddit.com/r/neovim/comments/1jmqd7t/sorry_ufo_these_7_lines_replaced_you/
+-- Nice and simple folding:
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ""
+o.foldcolumn = "0"
+o.fillchars:append({ fold = " " })
